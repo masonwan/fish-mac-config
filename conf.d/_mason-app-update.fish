d9c4b1739fb6a3fdcc9e42cd637ae4e4bbc49ece
Mason Wan <mwan@linkedin.com>
2017-01-11 10:58:37 -0800

    Support both yarn and npm to upgrade NPM modeuls

--- conf.d/_mason-app-update.fish
+++ conf.d/_mason-app-update.fish
@@ -1,4 +1,9 @@
 function app-update -d 'Update with Homebrew and Nodejs'
   brew update; and brew upgrade --cleanup; and brew cleanup
-  yarn global upgrade
+  if type -q yarn > /dev/null
+    yarn global upgrade
+  else
+    log warn 'Could not find yarn, upgrade modules with npm instead.'
+    npm -g update
+  end
 end
