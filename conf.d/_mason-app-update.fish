912b74c8fefa83856c4658cfb07ffeb55f289f60
Mason Wan <mwan@linkedin.com>
2017-01-31 09:01:17 -0800

    Add `fisher update` in `app-update`

--- conf.d/_mason-app-update.fish
+++ conf.d/_mason-app-update.fish
@@ -1,9 +1,14 @@
 function app-update -d 'Update with Homebrew and Nodejs'
   brew update; and brew upgrade --cleanup; and brew cleanup
+
   if type -q yarn > /dev/null
     yarn global upgrade
   else
     log warn 'Could not find yarn, upgrade modules with npm instead.'
     npm -g update
+  end
+
+  if type -q fisher > /dev/null
+    fisher update
   end
 end
