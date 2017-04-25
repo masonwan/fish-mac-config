ece8b4f8231c36bd39abce38d34d447ee0493a38
Mason Wan <mwan@linkedin.com>
2017-04-25 12:03:39 -0700

    lint-trash: Add capability to accept days from parameters

--- functions/lint-trash.fish
+++ functions/lint-trash.fish
@@ -1,11 +1,17 @@
 function lint-trash -d "Remove items older than 30 days in Trash"
-  set list (find ~/.Trash -maxdepth 1 -mtime +30)
+  if [ (count $argv) -ge 1 ]
+    set count $argv[1]
+  else
+    set count 30 # Default to 30 days
+  end
+
+  set list (find ~/.Trash -maxdepth 1 -mtime +$count)
   set file_count (count $list)
 
   if [ $file_count -eq 0 ]
-    log info 'Nothing to remove'
+    log info "Did not find trash older than $count days"
   else
-    log info 'Removing '$file_count' files'
+    log info "Removing $file_count files older than $count days"
     ls -AFlhdtr $list
     rm -rf $list
   end
