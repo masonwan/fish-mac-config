a8821655fcd7d3ebdfe772352070511a755b6bea
Mason Wan <mwan@linkedin.com>
2017-11-15 14:39:11 -0800

    Use yarn with silence mode

--- conf.d/_mason-app-update.fish
+++ conf.d/_mason-app-update.fish
@@ -2,7 +2,7 @@ function app-update -d 'Update with Homebrew and Nodejs'
   brew update; and brew upgrade --cleanup; and brew cleanup
 
   if type -q yarn > /dev/null
-    yarn global upgrade
+    yarn global upgrade --latest --caret --slient --no-progress > /dev/null
   end
 
   if type -q node > /dev/null
