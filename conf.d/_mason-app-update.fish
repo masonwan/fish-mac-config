4e5c096e840befbd81549cf7da2e6c24c65de17d
Mason Wan <mwan@linkedin.com>
2018-01-29 09:43:38 -0800

    Improve app-update script

--- conf.d/_mason-app-update.fish
+++ conf.d/_mason-app-update.fish
@@ -1,16 +1,24 @@
 function app-update -d 'Update with Homebrew and Nodejs'
-  brew update; and brew upgrade --cleanup; and brew cleanup
+  if type -q brew
+    log info 'Updating Homebrew modules...'
+    brew update
+    brew upgrade --cleanup
+    brew cleanup
+  end
 
   if type -q yarn > /dev/null
+    log info 'Updating yarn modules...'
     yarn global upgrade --latest --caret --slient --no-progress > /dev/null
   end
 
-  if type -q node > /dev/null
+  if type -q npm > /dev/null
+    log info 'Updating NPM modules...'
     npm -g install npm
     npm -g update
   end
 
   if type -q fisher > /dev/null
+    log info 'Updating fisherman modules...'
     fisher update
   end
 end
