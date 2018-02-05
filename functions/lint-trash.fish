679de504c6630c77ea227dbec1b67ed1709b3770
Mason Wan <mwan@linkedin.com>
2018-02-05 13:30:08 -0800

    lint-trash now uses access time instead of modify time

--- functions/lint-trash.fish
+++ functions/lint-trash.fish
@@ -5,13 +5,13 @@ function lint-trash -d "Remove items older than 30 days in Trash"
     set count 30 # Default to 30 days
   end
 
-  set list (find ~/.Trash -maxdepth 1 -mtime +$count)
+  set list (find ~/.Trash -maxdepth 1 -atime +$count)
   set file_count (count $list)
 
   if [ $file_count -eq 0 ]
     log info "Did not find trash older than $count days"
   else
-    log info "Removing $file_count files older than $count days"
+    log info "Removing $file_count trash files accessed more $count days ago"
     ls -AFlhdtr $list
     rm -rf $list
   end
